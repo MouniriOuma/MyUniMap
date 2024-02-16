7a107dff4638cb91d833d3745f6260149956d028
Lenovo <mouniri.ouma@gmail.com>
2024-02-16 22:38:22 +0000

    add wifi infos and styling

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -7,13 +7,16 @@ import { useNavigation } from '@react-navigation/native';
 const Profile = () => {
   const navigation = useNavigation();
   const [creator, setCreator] = useState('');
+  const [creatorEmail, setCreatorEmail] = useState('');
 
   useEffect(() => {
     const unsubscribe = firebaseAuth.onAuthStateChanged(user => {
       if (user) {
         // Extract username from email
         const username = user.email.split('@')[0];
+        const userEmail = user.email;
         setCreator(username);
+        setCreatorEmail(userEmail);
       }
     });
     return unsubscribe;
@@ -36,6 +39,15 @@ const Profile = () => {
             <Ionicons name="person-circle-outline" size={100} color="#007260" />
           </View>
           <Text style={styles.username}>{creator}</Text>
+          <Text style={styles.userEmail}>{creatorEmail}</Text>
+        </View>
+        <View>
+        <View style={styles.wifiCard}>
+      <Text style={styles.title}>WIFI Access information</Text>
+      <Text style={styles.text}>Network name: compus</Text>
+      <Text style={styles.text}>Email: your academic email</Text>
+      <Text style={styles.text}>Password: your ucd password</Text>
+    </View>
         </View>
       </View>
       <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
@@ -49,6 +61,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#fff',
   },
   card: {
@@ -74,6 +87,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#007260',
   },
+  userEmail: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#222222',
+  },
   logoutButton: {
     backgroundColor: '#39B68D',
     paddingVertical: 10,
@@ -84,6 +102,29 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  wifiCard: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    margin: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 4,
   },
 });
 
