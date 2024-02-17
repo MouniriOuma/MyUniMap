246991dc513f3afb21a997e5818c9645c9afb006
Lenovo <mouniri.ouma@gmail.com>
2024-02-17 03:54:54 +0000

    tab icons

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -7,6 +7,7 @@ import EventList from '../screens/EventListScreen';
 import AddEvent from '../screens/AddEvent';
 import Profile from '../screens/Profile';
 import TutorialScreen1 from '../screens/TutorialScreen1';
+import  Ionicons  from 'react-native-vector-icons/Ionicons';
 
 
 const Stack = createNativeStackNavigator();
@@ -15,9 +16,34 @@ const Tab = createBottomTabNavigator();
 const AppTabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Events" component={EventList} />
-      <Tab.Screen name="UniversityMap" component={UniversityMap} />
-      <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }}/>
+      <Tab.Screen 
+        name="Events" 
+        component={EventList} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tab.Screen 
+        name="UniversityMap" 
+        component={UniversityMap} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tab.Screen 
+        name="Profile" 
+        component={Profile} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+          headerShown: false
+        }} 
+      />
     </Tab.Navigator>
   );
 };
